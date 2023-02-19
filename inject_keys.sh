f9ef4c96a76383dc9ee0f428db0431bd1c38b284
bernardo <bernardo@parity.io>
2023-02-19 17:29:23 -0300

    add inject_keys.sh

---
+++ inject_keys.sh
@@ -0,0 +1,8 @@
+curl --location --request POST 'http://localhost:9933' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+"jsonrpc": "2.0",
+"method": "author_insertKey",
+"params": ["btc!","clip organ olive upper oak void inject side suit toilet stick narrow","0x9effc1668ca381c242885516ec9fa2b19c67b6684c02a8a3237b6862e5c8cd7e"],
+"id": 1
+}'
